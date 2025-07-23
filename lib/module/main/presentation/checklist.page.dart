e327f73f0da98f12c190ecb2cea20dc8e76546c8
jeeesher <bulaonronanne@gmail.com>
2025-07-23 12:35:41 +0800

    Added app bar

--- lib/module/main/presentation/checklist.page.dart
+++ lib/module/main/presentation/checklist.page.dart
@@ -7,16 +7,36 @@ class ChecklistPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Checklist Page'),
+        backgroundColor: Color(0xFF4A3780),
+        title: const Text('My To-Do List',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+            )),
+        actions: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.delete_outline_rounded, color: Color(0xFF4A3780)),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('All tasks cleared!')),
+                );
+              },
+              tooltip: 'Clear All Tasks',
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'This is the checklist page.',
-            ),
-          ],
+          children: <Widget>[const Text('This is the checklist page.')],
         ),
       ),
     );

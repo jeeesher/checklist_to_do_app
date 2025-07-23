eff2cb16d4fab4c7e0188d0f087a0229a33981ea
jeeesher <bulaonronanne@gmail.com>
2025-07-23 20:54:59 +0800

    Created TaskEntity

---
+++ lib/module/task/domain/entity/task.entity.dart
@@ -0,0 +1,23 @@
+class TaskEntity {
+  final String id;
+  final String title;
+  final bool isCompleted;
+
+  const TaskEntity({
+    required this.id,
+    required this.title,
+    required this.isCompleted,
+  });
+
+  TaskEntity copyWith({
+    String? id,
+    String? title,
+    bool? isCompleted,
+  }) {
+    return TaskEntity(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+}

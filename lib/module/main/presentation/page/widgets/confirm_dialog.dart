9901ce6604d9327c4a3bdd0e9ce5d1ad6dd04b64
jeeesher <bulaonronanne@gmail.com>
2025-07-23 18:00:59 +0800

    Made the dialog modal reusable

--- lib/module/main/presentation/page/widgets/confirm_dialog.dart
+++ lib/module/main/presentation/page/widgets/confirm_dialog.dart
@@ -1,25 +1,48 @@
-import 'package:checklist_to_do_app/module/task/domain/cubit/task_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ConfirmDialog extends StatelessWidget {
-  static Future<void> show(BuildContext context) {
+  final String title;
+  final String message;
+  final String confirmText;
+  final VoidCallback onConfirm;
+
+  const ConfirmDialog({
+    super.key,
+    required this.title,
+    required this.message,
+    required this.onConfirm,
+    this.confirmText = 'Confirm',
+  });
+
+  static Future<void> show({
+    required BuildContext context,
+    required String title,
+    required String message,
+    required VoidCallback onConfirm,
+    String confirmText = 'Confirm',
+  }) {
     return showDialog<void>(
       context: context,
-      builder: (dialogContext) => ConfirmDialog._(),
+      builder: (_) => ConfirmDialog(
+        title: title,
+        message: message,
+        onConfirm: onConfirm,
+        confirmText: confirmText,
+      ),
     );
   }
-
-  const ConfirmDialog._();
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('Clear All Tasks'),
-      content: Text(
-        'Are you sure you want to delete all tasks? This action cannot be undone.',
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
       ),
+      backgroundColor: Colors.white,
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text(message),
       actions: [
+        // Cancel button
         ElevatedButton(
           onPressed: () => Navigator.pop(context),
           style: ElevatedButton.styleFrom(
@@ -27,20 +50,24 @@ class ConfirmDialog extends StatelessWidget {
               borderRadius: BorderRadius.circular(30),
             ),
           ),
-          child: Text('Cancel', style: TextStyle(color: Color(0xFF4A3780))),
+          child: const Text(
+            'Cancel',
+            style: TextStyle(color: Color(0xFF4A3780)),
+          ),
         ),
+
         ElevatedButton(
           onPressed: () {
-            context.read<TaskCubit>().clearAllTasks();
+            onConfirm(); 
             Navigator.pop(context);
           },
           style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xFF4A3780),
+            backgroundColor: Color(0xFF4A3780),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(30),
             ),
           ),
-          child: Text('Clear All', style: TextStyle(color: Colors.white)),
+          child: Text(confirmText, style: const TextStyle(color: Colors.white)),
         ),
       ],
     );

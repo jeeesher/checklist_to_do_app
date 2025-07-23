d9caba0b0d507e8944262d65b9b7779773c75652
jeeesher <bulaonronanne@gmail.com>
2025-07-23 14:55:55 +0800

    Created model for Task

---
+++ lib/module/task/data/model/task.model.dart
@@ -0,0 +1,39 @@
+class Task {
+  final String id;
+  final String title;
+  final bool isCompleted;
+
+  Task({
+    required this.id,
+    required this.title,
+    this.isCompleted = false,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+      isCompleted: map['isCompleted'] ?? false,
+    );
+  }
+
+  Task copyWith({
+    String? id,
+    String? title,
+    bool? isCompleted,
+  }) {
+    return Task(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+}

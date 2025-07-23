885ffac0b847e380cb5750c1d90feff85a8414c1
jeeesher <bulaonronanne@gmail.com>
2025-07-23 12:54:50 +0800

    Added a dummy list of task for layout

--- lib/module/main/presentation/checklist.page.dart
+++ lib/module/main/presentation/checklist.page.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ChecklistPage extends StatelessWidget {
-  const ChecklistPage({super.key});
+  ChecklistPage({super.key});
+
+  final List<Map<String, dynamic>> dummyTasks = [
+    {'title': 'Buy groceries', 'isCompleted': false},
+    {'title': 'Walk the dog', 'isCompleted': true},
+    {'title': 'Finish Flutter exam', 'isCompleted': false},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFEFF2F5),
       appBar: AppBar(
         backgroundColor: Color(0xFF4A3780),
         title: const Text('My To-Do List',
@@ -33,11 +40,52 @@ class ChecklistPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[const Text('This is the checklist page.')],
-        ),
+      body: dummyTasks.isEmpty
+          ? _buildEmptyState()
+          : ListView.builder(
+              padding: const EdgeInsets.all(20),
+              itemCount: dummyTasks.length,
+              itemBuilder: (context, index) {
+                final task = dummyTasks[index];
+                return Card(
+                  color: Colors.white,
+                  elevation: 0.5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.only(bottom: 8),
+                  child: ListTile(
+                    leading: Checkbox(
+                      value: task['isCompleted'],
+                      onChanged: (value) {
+                        // No functionality yet
+                      },
+                      activeColor: Color(0xFF4A3780),
+                      checkColor: Colors.white,
+                    ),
+                    title: Text(
+                      task['title'],
+                      style: TextStyle(
+                        decoration: task['isCompleted']
+                            ? TextDecoration.lineThrough
+                            : null,
+                        color:
+                            task['isCompleted'] ? Colors.grey : Colors.black,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+
+  Widget _buildEmptyState() {
+    return Center(
+      child: Text(
+        'No tasks available. Tap the "+" button to add a new task.',
+        style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+        textAlign: TextAlign.center,
       ),
     );
   }

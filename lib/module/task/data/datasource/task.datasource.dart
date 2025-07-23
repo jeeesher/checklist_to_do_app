b9c01e513550537f18982622c738aac554ba4e39
jeeesher <bulaonronanne@gmail.com>
2025-07-23 23:14:56 +0800

    Renamed a variable

--- lib/module/task/data/datasource/task.datasource.dart
+++ lib/module/task/data/datasource/task.datasource.dart
@@ -6,8 +6,8 @@ class TaskDataSource {
   static const String _tasksKey = 'tasks';
 
   Future<List<TaskModel>> loadTasks() async {
-    final prefs = await SharedPreferences.getInstance();
-    final tasksJson = prefs.getStringList(_tasksKey) ?? [];
+    final taskPreference = await SharedPreferences.getInstance();
+    final tasksJson = taskPreference.getStringList(_tasksKey) ?? [];
 
     return tasksJson.map((jsonString) {
       final map = json.decode(jsonString) as Map<String, dynamic>;
@@ -16,14 +16,14 @@ class TaskDataSource {
   }
 
   Future<void> saveTasks(List<TaskModel> tasks) async {
-    final prefs = await SharedPreferences.getInstance();
+    final taskPreference = await SharedPreferences.getInstance();
     final tasksJson =
         tasks.map((task) => json.encode(task.toMap())).toList();
-    await prefs.setStringList(_tasksKey, tasksJson);
+    await taskPreference.setStringList(_tasksKey, tasksJson);
   }
 
   Future<void> clearAllTasks() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(_tasksKey);
+    final taskPreference = await SharedPreferences.getInstance();
+    await taskPreference.remove(_tasksKey);
   }
 }

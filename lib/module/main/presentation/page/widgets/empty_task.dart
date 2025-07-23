d1c3168c762469a16925336346159bd50bc1a111
jeeesher <bulaonronanne@gmail.com>
2025-07-23 16:02:28 +0800

    Fixed UI layout

--- lib/module/main/presentation/page/widgets/empty_task.dart
+++ lib/module/main/presentation/page/widgets/empty_task.dart
@@ -9,16 +9,16 @@ class EmptyTaskState extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: const [
-          Icon(Icons.checklist, size: 64, color: Colors.grey),
+          Icon(Icons.checklist, size: 50, color: Colors.grey),
           SizedBox(height: 16),
           Text(
             'No tasks yet!',
-            style: TextStyle(fontSize: 18, color: Colors.grey),
+            style: TextStyle(fontSize: 18, color: Colors.grey, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
           Text(
             'Add new task by clicking the button below.',
-            style: TextStyle(color: Colors.grey),
+            style: TextStyle(color: Colors.grey, fontSize: 16),
           ),
         ],
       ),

d22492230d2621eb7aabcf80b5d3ad3983a46577
jeeesher <bulaonronanne@gmail.com>
2025-07-23 12:55:49 +0800

    Moved the Checklist Page to a page folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:checklist_to_do_app/module/main/presentation/checklist.page.dart';
+import 'package:checklist_to_do_app/module/main/presentation/page/checklist.page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
